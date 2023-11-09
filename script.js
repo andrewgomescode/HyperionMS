9287d58652ffb50ccc056b4171822dad10cd8c10
andrewgomescode <andrewgomes.brc@gmail.com>
2023-11-09 11:40:50 -0300

    atualização

--- script.js
+++ script.js
@@ -1,58 +1,71 @@
-var explorer = document.getElementById("explorer");
-var modal = document.getElementById("modal");
-var modalimage = document.getElementById("modalimage");
-var modalcaption = document.getElementById("modalcaption");
-var closeBtn = document.querySelector(".close");
-var currentIndex = 0;
+var classList = ["explorer", "cygnusknights", "heroes", "resistance", "others"];
 
-explorer.addEventListener("click", function (event) {
-    if (event.target.tagName === "LI") {
-        var itemName = event.target.textContent;
-        var imageSrc = event.target.getAttribute("data-image");
+classList.forEach(function (className) {
+    var currentList = document.getElementById(className);
+    var modal = document.getElementById("modal" + className);
+    var modalImage = document.getElementById("modalimage" + className);
+    var modalCaption = document.getElementById("modalcaption" + className);
+    var closeBtn = document.querySelector("#modal" + className + " .close");
+    var prevArrow = document.querySelector("#modal" + className + " .prev");
+    var nextArrow = document.querySelector("#modal" + className + " .next");
 
-        if (itemName && imageSrc) {
-            modalimage.src = imageSrc;
-            modalcaption.textContent = itemName;
-            modal.style.display = "block";
+    var currentIndex = 0;
+
+    currentList.addEventListener("click", function (event) {
+        if (event.target.tagName === "LI") {
+            var itemName = event.target.textContent;
+            var imageSrc = event.target.getAttribute("data-image");
+
+            if (itemName && imageSrc) {
+                modalImage.src = imageSrc;
+                modalCaption.textContent = itemName;
+                modal.style.display = "block";
+            }
         }
-    }
-});
+    });
 
-modal.addEventListener("click", function (event) {
-    if (event.target === modal) {
+    closeBtn.addEventListener("click", function () {
         modal.style.display = "none";
-    }
-});
+    });
 
-document.addEventListener("keydown", function (event) {
-    if (event.key === "Escape") {
-        modal.style.display = "none";
-    } else if (event.key === "ArrowLeft") {
+    prevArrow.addEventListener("click", function () {
         plusSlides(-1);
-    } else if (event.key === "ArrowRight") {
+    });
+
+    nextArrow.addEventListener("click", function () {
         plusSlides(1);
+    });
+
+    document.addEventListener("keydown", function (event) {
+        if (event.key === "Escape") {
+            modal.style.display = "none";
+        } else if (event.key === "ArrowLeft") {
+            plusSlides(-1);
+        } else if (event.key === "ArrowRight") {
+            plusSlides(1);
+        }
+    });
+
+    function plusSlides(n) {
+        showSlide(currentIndex += n);
+    }
+
+    function showSlide(index) {
+        var items = document.querySelectorAll("#" + className + " li");
+
+        if (index >= items.length) {
+            currentIndex = 0; // Wrap around to the first item
+        } else if (index < 0) {
+            currentIndex = items.length - 1; // Wrap around to the last item
+        } else {
+            currentIndex = index;
+        }
+
+        var selectedListItem = items[currentIndex];
+        var itemName = selectedListItem.textContent;
+        var imageSrc = selectedListItem.getAttribute("data-image");
+
+        modalImage.src = imageSrc;
+        modalCaption.textContent = itemName;
     }
 });
-
-function plusSlides(n) {
-    showSlide(currentIndex += n);
-}
-
-function showSlide(index) {
-    var items = document.querySelectorAll("#explorer li");
-
-    if (index >= items.length) {
-        currentIndex = 0; // Wrap around to the first item
-    } else if (index < 0) {
-        currentIndex = items.length - 1; // Wrap around to the last item
-    } else {
-        currentIndex = index;
-    }
-
-    var selectedListItem = items[currentIndex];
-    var itemName = selectedListItem.textContent;
-    var imageSrc = selectedListItem.getAttribute("data-image");
-
-    modalimage.src = imageSrc;
-    modalcaption.textContent = itemName;
-}
