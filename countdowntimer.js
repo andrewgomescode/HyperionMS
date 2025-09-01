53ae4a2a195fc3fa63cf1daba64d81c0b3ba666c
Andrew Gomes <andrewgomes.brc@gmail.com>
2025-09-01 18:07:16 -0300

    Update countdowntimer.js

--- countdowntimer.js
+++ countdowntimer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dez 25, 2025 00:00:00").getTime();
+var countDownDate = new Date(2025, 11, 25, 0, 0, 0).getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function () {
